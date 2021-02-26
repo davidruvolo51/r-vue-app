046f6008eae1dbfa8064e2e9519277301ef9ebe3
davidruvolo51 <davidruvolo51@gmail.com>
2021-02-26 17:39:08 +0100

    init repo structure

---
+++ dev/dev.R
@@ -0,0 +1,14 @@
+#'////////////////////////////////////////////////////////////////////////////
+#' FILE: dev.R
+#' AUTHOR: David Ruvolo
+#' CREATED: 2021-02-26
+#' MODIFIED: 2021-02-26
+#' PURPOSE: workspace management
+#' STATUS: in.progress
+#' PACKAGES: NA
+#' COMMENTS: NA
+#'////////////////////////////////////////////////////////////////////////////
+
+# renv
+renv::init(bare = TRUE)
+renv::install("")
