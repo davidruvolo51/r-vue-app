ef94cdf8e297c6580ba521045af3547ebb45156b
davidruvolo51 <davidruvolo51@gmail.com>
2023-05-12 16:38:08 +0200

    adjusted indentation

--- server/server.R
+++ server/server.R
@@ -2,9 +2,9 @@
 #' FILE: server.R
 #' AUTHOR: David Ruvolo
 #' CREATED: 2021-03-02
-#' MODIFIED: 2021-03-02
+#' MODIFIED: 2023-05-12
 #' PURPOSE: start R plumber
-#' STATUS: in.progress
+#' STATUS: stable
 #' PACKAGES: plumber; palmerpenguis
 #' COMMENTS: NA
 #'////////////////////////////////////////////////////////////////////////////
@@ -18,24 +18,24 @@ app$mount("/", plumber::PlumberStatic$new("./client/dist"))
 
 #* @filter cors
 cors <- function(req, res) {
-    res$setHeader("Access-Control-Allow-Origin", "*")
-    res$setHeader("Access-Control-Allow-Methods", "*")
-    res$setHeader("Access-Control-Allow-Headers", "Content-Type")
-    plumber::forward()
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  res$setHeader("Access-Control-Allow-Methods", "*")
+  res$setHeader("Access-Control-Allow-Headers", "Content-Type")
+  plumber::forward()
 }
 
 
 # endpoint: `/api/data`
 app$handle(
-    methods = "GET",
-    path = "/api/data",
-    serializer = plumber::serializer_json(
-        na = "string",
-        type = "application/json"
-    ),
-    handler = function(req) {
-        list(json = palmerpenguins::penguins[1:10, ])
-    }
+  methods = "GET",
+  path = "/api/data",
+  serializer = plumber::serializer_json(
+    na = "string",
+    type = "application/json"
+  ),
+  handler = function(req) {
+    list(items = palmerpenguins::penguins[1:10, ])
+  }
 )
 
 
