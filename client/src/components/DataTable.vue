3be490c17f8d2ff972c27aefd4f0d41e20dd99a7
davidruvolo51 <davidruvolo51@gmail.com>
2023-05-12 16:39:09 +0200

    updated prop typing

--- client/src/components/DataTable.vue
+++ client/src/components/DataTable.vue
@@ -15,40 +15,54 @@
   </table>
 </template>
 
-<script setup>
-import { defineProps } from "vue";
-defineProps({
-  data: Array,
-  colnames: Array,
-});
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    colnames: {
+      type: Array,
+      required: true
+    }
+  }
+}
+
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .datatable {
   font-family: Arial, Helvetica, sans-serif;
   width: 100%;
   border-spacing: 0;
   text-align: left;
   font-size: 13pt;
+  
+  thead {
+    tr {
+      th {
+        font-weight: 600;
+        padding: 4px 12px;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        border-bottom: 1px solid #252525;
+        color: #252525;
+      }
+    }
+  }
+  
+  tbody {
+    tr {
+      td, th {
+        font-weight: 400;
+        padding: 16px 12px;
+      }
+      
+      &:nth-child(even) {
+        background-color: #EEF8FC;
+      }
+    }
+  }
 }
-
-.datatable thead tr th {
-  font-weight: 600;
-  padding: 4px 12px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  border-bottom: 1px solid #252525;
-  color: #252525;
-}
-
-.datatable tbody tr td,
-.datatable tbody tr th {
-  font-weight: 400;
-  padding: 16px 12px;
-}
-
-.datatable tbody tr:nth-child(even) {
-  background-color: #EEF8FC;
-}
-
 </style>
